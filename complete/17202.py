6aaf0f40912a3beea4d0876d13dbb0995dd28a22
JaeWon <chjwon10044@gmail.com>
2020-08-06 09:06:54 -0500

    17202 complete

--- complete/17202.py
+++ complete/17202.py
@@ -17,5 +17,23 @@ phone_num2 = int(input())
 num1_list = numbers_split(phone_num1)
 num2_list = numbers_split(phone_num2)
 
-print(num1_list)
-print(num2_list)
+# print(num1_list)
+# print(num2_list)
+
+num_list = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+# print(len(num_list))
+for k in range (8):
+    num_list[2*k] = num1_list[k]
+    num_list[2*k+1] = num2_list[k]
+
+# print(num_list)
+for tt in range(14):
+    for temp in range(15):
+        num_list[temp] = (num_list[temp]+num_list[temp + 1])%10
+
+
+# print(num_list[0])
+# print(num_list[1])
+
+print(str(num_list[0])+str(num_list[1]))
